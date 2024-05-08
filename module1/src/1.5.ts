efa582e9698a5850dffe3b0433756812833d17d6
shahehsan2002 <shahehsan2002@.com>
2024-05-08 13:56:22 +0600

    object,optional and literal types

---
+++ module1/src/1.5.ts
@@ -0,0 +1,16 @@
+// Reference Type --> Object
+
+const  user :{
+    firstName : string;
+    middleName ? : string;
+    lastName : string;
+    isStudent: boolean;
+    // type --> literal types
+    institue:'Programming Hero';  
+} ={
+    firstName:'Shah',
+    middleName:'Ehsanul',
+    lastName:'Haque',
+    isStudent:true,
+    institue:'Programming Hero',
+}
