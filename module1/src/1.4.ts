9639a04042d204db99e0e495caf50450e2ecc521
shahehsan2002 <shahehsan2002@.com>
2024-05-08 13:28:24 +0600

    frist commit

---
+++ module1/src/1.4.ts
@@ -0,0 +1,24 @@
+                            // Basic Data type ---
+
+// string
+let firstName : string = "Ehsan";
+
+// number
+let roll : number = 123;
+
+// boolean 
+let isadmin : boolean = true;
+
+// undefined 
+let x : undefined = undefined;
+
+// null
+let y :null = null;
+
+// Array
+let friends :string[] = ['nut', 'boltu'];
+let eligibleRoleList : number[] =[1,3,];
+
+// tuple --> array --> order --> type of values
+let coordinates: [number,number] =[1,2];
+let ageName : [number,string,boolean] = [20,'mr x', true];
